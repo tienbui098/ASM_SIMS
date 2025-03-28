4ab2c5b9108e20e704c0e944fdc7bff58cb65406
Pham Quang Huy <chipco14092005@gmail.com>
2025-03-28 16:10:51 +0700

    update Factory

---
+++ SIMS_ASM/Factory/ClassRepository.cs
@@ -0,0 +1,22 @@
+﻿using SIMS_ASM.Data;
+using SIMS_ASM.Models;
+
+namespace SIMS_ASM.Factory
+{
+    public class ClassRepository : Repository<Class>
+    {
+        private ApplicationDbContex _context;
+        public ClassRepository(ApplicationDbContex context) : base(context)
+        {
+            this._context = context;
+        }
+
+        // Additional class-specific methods
+        public IEnumerable<Class> GetClassesByMajor(int majorId)
+        {
+            return _context.Classes
+                .Where(c => c.MajorID == majorId)
+                .ToList();
+        }
+    }
+}
