7d9f43304673a2f59e3f3929f248fea587c83f0d
tienbui098 <btien098@gmail.com>
2025-03-28 17:50:02 +0700

    Edit view and add Services for Course

---
+++ SIMS_ASM/Services/CourseService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIMS_ASM.Data;
+using SIMS_ASM.Models;
+
+namespace SIMS_ASM.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ApplicationDbContex _context;
+
+        public CourseService(ApplicationDbContex context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Course>> GetAllCoursesAsync()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        public async Task<Course> GetCourseByIdAsync(int id)
+        {
+            return await _context.Courses.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Course>> GetCoursesByMajorAsync(int majorId)
+        {
+            return await _context.Courses.Where(c => c.MajorID == majorId).ToListAsync();
+        }
+
+        public async Task<Course> CreateCourseAsync(Course course)
+        {
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync();
+            return course;
+        }
+
+        public async Task UpdateCourseAsync(Course course)
+        {
+            _context.Entry(course).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCourseAsync(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course != null)
+            {
+                _context.Courses.Remove(course);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ SIMS_ASM/Services/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using SIMS_ASM.Models;
+
+namespace SIMS_ASM.Services
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<Course>> GetAllCoursesAsync();
+        Task<Course> GetCourseByIdAsync(int id);
+        Task<IEnumerable<Course>> GetCoursesByMajorAsync(int majorId);
+        Task<Course> CreateCourseAsync(Course course);
+        Task UpdateCourseAsync(Course course);
+        Task DeleteCourseAsync(int id);
+    }
+}
