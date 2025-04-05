d9351e87b7d9048c731bbec99ea974f8f1b533c5
Pham Quang Huy <chipco14092005@gmail.com>
2025-04-05 22:09:03 +0700

    Update Design Pattern Factory to Repository

---
+++ SIMS_ASM/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace SIMS_ASM.Factory
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void SaveChanges();
+    }
+}
